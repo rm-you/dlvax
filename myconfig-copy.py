ca421b01f367eca39f187f6d7b90e7bc6ea7aa80
Elliott Jackson <elliott.w.jackson@gmail.com>
2022-06-19 13:52:37 -0700

    created branch to get EverquestLogfile spruced up and ready for use

---
+++ myconfig-copy.py
@@ -0,0 +1,13 @@
+
+# configure location of the everquest log files
+BASE_DIRECTORY              = 'c:\\users\\ewjax\\Documents\\Gaming\\Everquest-Project1999'
+DEFAULT_CHAR_NAME           = 'Azleep'
+
+LOGS_DIRECTORY              = '\\logs\\'
+SERVER_NAME                 = 'P1999Green'
+
+# used in hearbeat test.  Max number of seconds of inactivity before it checks to see if a new file is being written to
+HEARTBEAT                   = 15
+
+
+
