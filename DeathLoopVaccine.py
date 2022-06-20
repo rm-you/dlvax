2b0c5a434483ecad6465ca428d7d62d745305d9f
Elliott Jackson <elliott.w.jackson@gmail.com>
2022-06-19 20:10:25 -0700

    EverquestLogFile is derived from thread

--- DeathLoopVaccine.py
+++ DeathLoopVaccine.py
@@ -1,7 +1,35 @@
 
 
 #
-# Prevent Death Loops
+# simple utility to prevent Everquest Death Loops
 #
 # parses log file, and if it detects D deaths in M minutes, and no player activity in between, it 
-# will kill 
+# will kill the eqgame.exe process
+#
+
+import EverquestLogFile
+
+class DeathLoopVaccine(EverquestLogFile.EverquestLogFile):
+
+    #
+    # ctor
+    #
+    def __init__(self):
+
+        # parent ctor
+        EverquestLogFile.EverquestLogFile.__init__(self)
+
+    def process_line(self, line):
+        print('*' + line, end='')
+
+def main():
+
+
+    dlv = DeathLoopVaccine()
+    dlv.begin_parsing()
+
+
+
+
+if __name__ == '__main__':
+    main()
